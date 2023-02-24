d158d4007b17ef68574d7957682f84a6f093e5fa
kitkath <katherinpgr_99@hotmail.com>
2023-02-23 21:41:00 -0500

    basically nothing

---
+++ bst.js
@@ -0,0 +1,14 @@
+class Node {
+  constructor(node, left = null, right = null) {
+    this.node = node
+    this.left = left
+    this.right = right
+  }
+}
+
+class Tree {
+  
+  buildTree(array) {
+    
+  }
+}
