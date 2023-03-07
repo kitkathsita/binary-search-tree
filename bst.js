ba3dbf22942335b81a4fe69290f5b18830987dcd
kitkath <katherinpgr_99@hotmail.com>
2023-03-06 21:20:55 -0500

    it creates the BST

--- bst.js
+++ bst.js
@@ -9,6 +9,42 @@ class Node {
 class Tree {
   
   buildTree(array) {
+    let newArray = Array.from(new Set(array.sort((a, b) => a-b)))
+
+    function createTree(sortArray) {
+      let start = 0
+      let end = sortArray.length - 1
+      let mid = Math.ceil((start+end)/2)
+      if (start>end) {
+        return null
+      } else {
+        let arrayRight = sortArray.splice(mid+1)
+        let arrayLeft = sortArray.splice(0, mid)
+        
+        let root = new Node(sortArray[0], createTree(arrayLeft), createTree(arrayRight))
+        return root
+      }
+    }
+    return createTree(newArray)
+  }
+
+  insert(number) {
     
   }
 }
+
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  }
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.node}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  }
+}
+
+let a = [11,5,4,8,0,15,6,7]
+let nodo = new Tree()
+console.log(nodo.buildTree(a))
+console.log('hola guerre')
+prettyPrint(nodo.buildTree(a))
