b5d1384a4c1f260aae81c3dbd7e2dd7a0225cf0c
kitkath <katherinpgr_99@hotmail.com>
2023-03-19 10:34:14 -0500

    this is the clean version, missing the delete method

--- bst.js
+++ bst.js
@@ -40,11 +40,6 @@ class Tree {
     return currentNode
   }
 
-  /* delete(number, currentNode = this.root) {
-    currentNode.node === number
-    ? 
-  } */
-
   find(number, currentNode = this.root) {
     if (currentNode === null) return null 
     if (currentNode.node === number) {
@@ -56,6 +51,109 @@ class Tree {
     } 
   }
 
+  levelOrder(func = 'none') {
+    let queue = [this.root]
+    let arr = []
+    while (queue.length > 0) {
+      if (queue[0] != null) {
+        let firstOne = queue.shift()
+        func === 'none'
+        ? arr.push(firstOne.node)
+        : func(firstOne)
+        queue.push(firstOne.left, firstOne.right)
+      } else {
+        queue.shift()
+      }
+    }
+    return arr
+  }
+
+  preOrder(actNode = this.root, arr = [], func = 'none') {
+    if (actNode === null) return
+    func === 'none'
+    ? arr.push(actNode.node)
+    : func(actNode.node)
+    this.inOrder(actNode.left, arr)
+    this.inOrder(actNode.right, arr)
+
+    return arr
+  }
+
+  inOrder(actNode = this.root, arr = [], func = 'none') {
+    if (actNode === null) return
+    this.inOrder(actNode.left, arr)
+    func === 'none'
+    ? arr.push(actNode.node)
+    : func(actNode.node)
+    this.inOrder(actNode.right, arr)
+
+    return arr
+  }
+
+  postOrder(actNode = this.root, arr = [], func = 'none') {
+    if (actNode === null) return
+    this.inOrder(actNode.left, arr)
+    this.inOrder(actNode.right, arr)
+    func === 'none'
+    ? arr.push(actNode.node)
+    : func(actNode.node)
+
+    return arr
+  }
+
+  heightTree(actNode = this.root) {
+    if (actNode === null) return -1
+
+    let leftHeight = this.heightTree(actNode.left)
+    let rightHeight = this.heightTree(actNode.right)
+
+    return Math.max(leftHeight, rightHeight) + 1
+  }
+  
+  height(number, actNode = this.find(number)) {
+    if (actNode === null) return -1
+
+    let leftHeight = this.height(actNode.left)
+    let rightHeight = this.height(actNode.right)
+
+    return Math.max(leftHeight, rightHeight) + 1
+  }
+
+  depth(number, root = this.root, edge = 0) {
+    if (root === null) return
+    if (root.node === number) return edge
+
+    if (root.node > number) {
+      return this.depth(number, root.left, edge + 1)
+    } else {
+      return this.depth(number, root.right, edge + 1)
+    }
+  }
+
+  isBalanced(actNode = this.root, prove = 'is balanced', list = []) {
+    if (actNode === null) return
+    let rightSubTree = this.heightTree(actNode.right)
+    let leftSubTree = this.heightTree(actNode.left)
+    
+    Math.abs(rightSubTree-leftSubTree) <= 1
+    ? prove = 'is balanced'
+    : prove = 'is not balanced'
+
+    list.push(prove)
+
+    this.isBalanced(actNode.right, prove, list)
+    this.isBalanced(actNode.left, prove, list)
+    
+    return list.includes('is not balanced') === true
+    ? 'tree is not balanced'
+    : 'tree is balanced'
+  }
+
+  rebalance() {
+    let arr = this.inOrder()
+    this.root = this.buildTree(arr)
+    return this.root
+  }
   
 }
 
@@ -72,8 +170,15 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
 let a = [11,5,4,8,0,15,6,7]
 let nodo = new Tree(a)
 nodo.insert(2)
-nodo.insert(17)
-console.log(nodo.find(6))
+nodo.insert(17) 
+/*console.log(nodo.find(6))
 /* console.log(nodo) */
 console.log('hola guerre')
+/* console.log(nodo.preOrder2())*/
 prettyPrint(nodo.root) 
+/*console.log(nodo.depth(6))*/
+console.log(nodo.isBalanced())
+nodo.rebalance()
+prettyPrint(nodo.root)
+console.log(nodo.height(10))
+console.log(nodo.isBalanced())
